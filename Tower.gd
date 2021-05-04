78cbd39ec0bf4f44c4970419b5da8001817e41d1
Max <Code@stilke.net>
2021-05-05 00:12:00 +0200

    bug: same Sound on every Tower

--- Tower.gd
+++ Tower.gd
@@ -4,7 +4,7 @@ var Bullet= preload("res://Bullet.tscn")
 var initialPos
 var sound
 var number = 0
-var sounds = [load("res://Assets/Sounds/Amaj1.ogg"), load("res://Assets/Sounds/Amaj2.ogg"), load("res://Assets/Sounds/Amaj3.ogg"), load("res://Assets/Sounds/Amaj4.ogg"), load("res://Assets/Sounds/Amaj5.ogg")]
+var sounds = ["res://Assets/Sounds/Amaj2.wav", "res://Assets/Sounds/Amaj7.ogg", "res://Assets/Sounds/Amaj3.ogg", "res://Assets/Sounds/Amaj4.ogg", "res://Assets/Sounds/Amaj5.ogg"]
 var key
 var size = OS.window_size
 #var positions = [Vector2(number*100, size.y-number*35), Vector2(200,500), Vector2(600,100), Vector2(500,200), Vector2(400,300), Vector2(300,400),  Vector2(600, 0)]
@@ -12,7 +12,7 @@ var size = OS.window_size
 func loadSound(a):
 	var player = AudioStreamPlayer.new()
 	add_child(player)
-	player.stream = sounds[a]
+	player.stream = load(sounds[a])
 	sound = player
 	
 func _ready():
@@ -26,8 +26,8 @@ func initialize(num):
 	key = number_to_key(num)
 	loadSound(num)
 	
-func number_to_key(number):
-	match number:
+func number_to_key(n):
+	match n:
 		1: return "a"
 		2: return "s"
 		3: return "d"
