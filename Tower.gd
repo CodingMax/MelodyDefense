2328302322eb2eccc7d4fa78668f9178c1b1b791
Max <Code@stilke.net>
2021-05-05 01:11:12 +0200

    feat: pitch shift

--- Tower.gd
+++ Tower.gd
@@ -4,27 +4,31 @@ var Bullet= preload("res://Bullet.tscn")
 var initialPos
 var sound
 var number = 0
-var sounds = ["res://Assets/Sounds/Amaj2.wav", "res://Assets/Sounds/Amaj7.ogg", "res://Assets/Sounds/Amaj3.ogg", "res://Assets/Sounds/Amaj4.ogg", "res://Assets/Sounds/Amaj5.ogg"]
 var key
 var size = OS.window_size
+
 #var positions = [Vector2(number*100, size.y-number*35), Vector2(200,500), Vector2(600,100), Vector2(500,200), Vector2(400,300), Vector2(300,400),  Vector2(600, 0)]
+
+var sounds = ["res://Assets/Sounds/Amaj2.wav", "res://Assets/Sounds/Amaj7.ogg", "res://Assets/Sounds/Amaj3.ogg", "res://Assets/Sounds/Amaj4.ogg", "res://Assets/Sounds/Amaj5.ogg"]
 
 func loadSound(a):
 	var player = AudioStreamPlayer.new()
 	add_child(player)
 	player.stream = load(sounds[a])
 	sound = player
-	
-func _ready():
-	initialPos = self.global_position 
-	initialize(number)
-	
-	
+	#var pitch = AudioEffectPitchShift
+	#pitch.set_pitch_scale(a)
+	sound.pitch_scale =  a*0.2 + 0.5
 func initialize(num):
-	num += 1
+	#num += 1
 	self.global_position = Vector2(num*200, size.y - num*100)#positions[number]
 	key = number_to_key(num)
 	loadSound(num)
+	
+func _ready():
+	initialPos = self.global_position 
+	#initialize(number)
+	
 	
 func number_to_key(n):
 	match n:
