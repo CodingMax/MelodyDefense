99bcf198c26e147080d2eb289416b2a25d475924
Max <Code@stilke.net>
2021-05-04 21:47:03 +0200

    sound: notes

--- Tower.gd
+++ Tower.gd
@@ -8,14 +8,18 @@ extends StaticBody2D
 #export (PackedScene) var Bullet
 var Bullet= preload("res://Bullet.tscn") 
 var initialPos
-
+var sound
 func _ready():
 	initialPos = self.global_position 
+	var player = AudioStreamPlayer.new()
+	add_child(player)
+	player.stream = load("res://Assets/Sounds/Amaj1.ogg")
+	sound = player
 func shoot():
 	var b = Bullet.instance()
 	add_child(b)
-	b.global_position = initialPos
-	#- Vector2(5,0)
+	b.global_position = initialPos	#- Vector2(5,0)
+	sound.play()
 	print("Shot")
 # Called when the node enters the scene tree for the first time.
 
