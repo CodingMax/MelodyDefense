55716ecb3e7b059f26a200cafe627a8bfe035bf0
Max <Code@stilke.net>
2021-05-07 21:34:41 +0200

    feat: BPM Sync, Midi Input (WIP)

--- GameOver.gd
+++ GameOver.gd
@@ -2,5 +2,9 @@ extends PopupMenu
 
 
 func _ready():
-	add_check_item("Restart", 1)
-	pass
+	add_check_item("Restart")
+	add_check_item("Quit")
+	set_item_as_checkable(1, true)
+func _process(delta):
+	if is_item_checked(1):
+		print("Test")
