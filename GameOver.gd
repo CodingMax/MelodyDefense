6c27cd30f828da9c2eb4cf53ae26f5229f9ed510
Max <Code@stilke.net>
2021-05-11 20:44:59 +0200

    save: Broken

--- GameOver.gd
+++ GameOver.gd
@@ -4,7 +4,7 @@ extends PopupMenu
 func _ready():
 	add_check_item("Restart")
 	add_check_item("Quit")
-	set_item_as_checkable(1, true)
+	#set_item_as_checkable(1, true)
 func _process(delta):
 	if is_item_checked(1):
 		print("Test")
